fe9b7ba883cb64ece47375fc236e83049800f70c
Paul Horton <pahorton@asu.edu>
2021-07-08 10:00:48 -0700

    Draggable windows

--- assets/scripts/easter.js
+++ assets/scripts/easter.js
@@ -1,23 +1,8 @@
-maxZ = 29;
+const maxZ = 29;
+var alive = [];
+var to = null;
+const eggs = Array.prototype.slice.call(document.querySelectorAll('.easter'));
 
-eggs = Array.prototype.slice.call(document.querySelectorAll('.easter'));
-
-eggs.forEach(egg => {
-    button = egg.querySelectorAll('button')[0];
-    button.addEventListener("click", function() {
-        egg.style.display = 'none';
-    });
-});
-
-eggs.forEach(egg => {
-    egg.addEventListener("click", function(event) {
-        if(!(event.target instanceof HTMLButtonElement)) {
-            focus(egg);
-        }
-    });
-});
-
-alive = [];
 function spawn(egg) {
     if(!alive.includes(egg)) {
         alive.forEach(div => {
@@ -30,6 +15,7 @@ function spawn(egg) {
         alive.unshift(egg);
     }
 }
+
 function focus(egg) {
     if(alive.includes(egg)) {
         index = alive.indexOf(egg);
@@ -39,6 +25,48 @@ function focus(egg) {
         alive.splice(index,1);
         alive.unshift(egg);
         egg.style.zIndex = maxZ;
+    }
+}
+
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+      // if present, the header is where you move the DIV from:
+      document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+      // otherwise, move the DIV from anywhere inside the DIV:
+      elmnt.onmousedown = dragMouseDown;
+    }
+  
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
     }
 }
 
@@ -58,9 +86,8 @@ function shuffle(array) {
     }
 
     return array;
-  }
-  
-var to = null;
+}
+
 function scramble() {
     if(to) {
         clearTimeout(to);
@@ -81,7 +108,19 @@ function scramble() {
     loop(0, eggs.length - 1);
 }
 
+eggs.forEach(egg => {
+    button = egg.querySelectorAll('button')[0];
+    button.addEventListener("click", function() {
+        egg.style.display = 'none';
+    });
+    egg.addEventListener("click", function(event) {
+        if(!(event.target instanceof HTMLButtonElement)) {
+            focus(egg);
+        }
+    });
+    
+});
+  
 document.getElementById('task-bar-logo').addEventListener("click", function(){
     scramble();
 });
-
